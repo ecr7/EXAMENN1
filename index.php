d4caf0d8d63db74850fb4487e34a2c0b4b733399
ecr7 <eclaudior@miumg.edu.gt>
2022-08-08 21:01:42 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examen</title>
+    <link rel="icon" href="icono.png">
+    <link rel="stylesheet" href="estilos.css">
+<style>
+    .contenedor{
+        display: flex;
+    }
+    
+    .text{
+        padding-left: 30px;
+    }
+
+</style>
+
+</head>
+
+<body>
+    <!--BARRA DE NAVEGACION-->
+
+    <nav>
+        <ul>
+            <li> <a href="index.php">Inicio</a> </li>
+
+            <li> <a href="tablas.html">Tablas</a> </li>
+
+            <li> <a href="formularios.html">Formularios</a></li>
+        </ul>
+    </nav>
+
+
+
+    <div class="contenedor">
+        <img src="imagen.jpg" alt="Imagen" width="500" height="333">
+        
+        <div class="text">
+       
+            <h1>BIENVENIDO</h1>
+      
+        <p >
+            Al examen parcial de desarollo Web, en las páginas que usted verá aquí podra ver el avance que hemos tenidos
+            en las primeras semanas de clase...
+        </p>
+    </div>
+        
+
+    </div>
+   
+    <footer>
+       Todos los derechos reservados a /UMG/
+    </footer>
+
+    
+</body>
+</html>
